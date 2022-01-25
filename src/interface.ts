bc543d71b63dd731a617e74c2d1abb710d773129
Harry Chen <czy88840616@gmail.com>
2022-01-25 18:03:29 +0800

    fix: write method output to error

--- src/interface.ts
+++ src/interface.ts
@@ -38,7 +38,7 @@ export interface IMidwayLogger extends ILogger {
   close(): any;
 }
 
-export type LoggerLevel = 'all' | 'silly' | 'debug' | 'info' | 'warn' | 'error' | 'none' | 'ALL' | 'DEBUG' | 'INFO' | 'WARN' | 'ERROR' | 'NONE';
+export type LoggerLevel = 'all' | 'silly' | 'debug' | 'info' | 'warn' | 'error' | 'none' | 'ALL' | 'DEBUG' | 'INFO' | 'WARN' | 'ERROR' | 'NONE' | 'trace' | 'TRACE';
 
 export interface LoggerOptions {
   format?: logform.Format;
